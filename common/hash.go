46feab6a95ccc585fd87131633207e1c52481d6a
Luke Plaster <me@lukep.org>
2019-06-18 22:11:48 +0800

    common: write inputs to hash in one array

--- common/hash.go
+++ common/hash.go
@@ -10,20 +10,34 @@ import (
 // https://en.wikipedia.org/wiki/Template:Comparison_of_SHA_functions
 func SHA512_256(in... []byte) ([]byte, error) {
 	state := crypto.SHA512_256.New()
+	size := 0
 	for _, bz := range in {
-		if _, err := state.Write(bz); err != nil {
-			return nil, err
-		}
+		size += len(bz)
+	}
+	data := make([]byte, 0, size)
+	for _, bz := range in {
+		data = append(data, bz...)
+	}
+	if _, err := state.Write(data); err != nil {
+		return nil, err
 	}
 	return state.Sum(nil), nil
 }
 
 func SHA512_256i(in... *big.Int) (*big.Int, error) {
 	state := crypto.SHA512_256.New()
-	for _, int := range in {
-		if _, err := state.Write(int.Bytes()); err != nil {
-			return nil, err
-		}
+	size := 0
+	ptrs := make([][]byte, len(in))
+	for i, int := range in {
+		ptrs[i] = int.Bytes()
+		size += len(ptrs[i])
+	}
+	data := make([]byte, 0, size)
+	for i := range in {
+		data = append(data, ptrs[i]...)
+	}
+	if _, err := state.Write(data); err != nil {
+		return nil, err
 	}
 	return new(big.Int).SetBytes(state.Sum(nil)), nil
 }
