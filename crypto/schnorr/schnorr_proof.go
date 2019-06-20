4b07130e98b0fe2e355be1acf78bba6eba72c517
Luke Plaster <me@lukep.org>
2019-06-20 21:18:01 +0800

    schnorr: add V proof funcs

--- crypto/schnorr/schnorr_proof.go
+++ crypto/schnorr/schnorr_proof.go
@@ -9,43 +9,101 @@ import (
 	"github.com/binance-chain/tss-lib/tss"
 )
 
-type SchnorrProof struct {
-	Alpha *crypto.ECPoint
-	T     *big.Int
-}
+type (
+	ZKProof struct {
+		Alpha *crypto.ECPoint
+		T     *big.Int
+	}
 
-// NewSchnorrProof can throw an error if writing the SHA512/256 hash fails
-func NewSchnorrProof(x *big.Int, X *crypto.ECPoint) *SchnorrProof {
+	ZKVProof struct {
+		Alpha *crypto.ECPoint
+		T,
+		U     *big.Int
+	}
+)
+
+// NewZKProof constructs a new Schnorr ZK proof of knowledge of the discrete logarithm (GG18Spec Fig. 16)
+func NewZKProof(x *big.Int, X *crypto.ECPoint) *ZKProof {
 	ecParams := tss.EC().Params()
 	q := ecParams.N
 	g := crypto.NewECPoint(ecParams.Gx, ecParams.Gy)
+
 	a := random.GetRandomPositiveInt(q)
 	alpha := crypto.ScalarBaseMult(tss.EC(), a)
 
-	cHash := common.SHA512_256i(X.X(), X.Y(), g.X(), g.Y(), alpha.X(), alpha.Y())
-	c := common.RejectionSample(q, cHash)
-
+	var c *big.Int
+	{ // must use RejectionSample
+		cHash := common.SHA512_256i(X.X(), X.Y(), g.X(), g.Y(), alpha.X(), alpha.Y())
+		c = common.RejectionSample(q, cHash)
+	}
 	t := new(big.Int).Mul(c, x)
 	t = new(big.Int).Add(a, t)
 	t = new(big.Int).Mod(t, q)
 
-	return &SchnorrProof{Alpha: alpha, T: t}
+	return &ZKProof{Alpha: alpha, T: t}
 }
 
-// Verify can throw an error if writing the SHA512/256 hash fails
-func (pf *SchnorrProof) Verify(X *crypto.ECPoint) bool {
+// NewZKProof verifies a new Schnorr ZK proof of knowledge of the discrete logarithm (GG18Spec Fig. 16)
+func (pf *ZKProof) Verify(X *crypto.ECPoint) bool {
 	ecParams := tss.EC().Params()
 	q := ecParams.N
 	g := crypto.NewECPoint(ecParams.Gx, ecParams.Gy)
 
-	cHash := common.SHA512_256i(X.X(), X.Y(), g.X(), g.Y(), pf.Alpha.X(), pf.Alpha.Y())
-	c := common.RejectionSample(q, cHash)
-
+	var c *big.Int
+	{ // must use RejectionSample
+		cHash := common.SHA512_256i(X.X(), X.Y(), g.X(), g.Y(), pf.Alpha.X(), pf.Alpha.Y())
+		c = common.RejectionSample(q, cHash)
+	}
 	tG := crypto.ScalarBaseMult(tss.EC(), pf.T)
 	Xc := X.ScalarMult(tss.EC(), c)
 	aXc := pf.Alpha.Add(tss.EC(), Xc)
 
 	if aXc.X().Cmp(tG.X()) != 0 || aXc.Y().Cmp(tG.Y()) != 0 {
+		return false
+	}
+	return true
+}
+
+// NewZKProof constructs a new Schnorr ZK proof of knowledge s_i, l_i such that V_i = R^s_i, g^l_i (GG18Spec Fig. 17)
+func NewZKVProof(V, R *crypto.ECPoint, s, l *big.Int) *ZKVProof {
+	ecParams := tss.EC().Params()
+	q := ecParams.N
+	g := crypto.NewECPoint(ecParams.Gx, ecParams.Gy)
+
+	a, b := random.GetRandomPositiveInt(q), random.GetRandomPositiveInt(q)
+	aR := R.ScalarMult(tss.EC(), a)
+	bG := crypto.ScalarBaseMult(tss.EC(), b)
+	alpha := aR.Add(tss.EC(), bG)
+
+	var c *big.Int
+	{ // must use RejectionSample
+		cHash := common.SHA512_256i(V.X(), V.Y(), R.X(), V.Y(), g.X(), g.Y(), alpha.X(), alpha.Y())
+		c = common.RejectionSample(q, cHash)
+	}
+	t := new(big.Int).Mod(new(big.Int).Add(a, new(big.Int).Mul(c, s)), q)
+	u := new(big.Int).Mod(new(big.Int).Add(b, new(big.Int).Mul(c, l)), q)
+
+	return &ZKVProof{Alpha: alpha, T: t, U: u}
+}
+
+func (pf *ZKVProof) Verify(V, R *crypto.ECPoint) bool {
+	ecParams := tss.EC().Params()
+	q := ecParams.N
+	g := crypto.NewECPoint(ecParams.Gx, ecParams.Gy)
+
+	var c *big.Int
+	{ // must use RejectionSample
+		cHash := common.SHA512_256i(V.X(), V.Y(), R.X(), R.Y(), g.X(), g.Y(), pf.Alpha.X(), pf.Alpha.Y())
+		c = common.RejectionSample(q, cHash)
+	}
+	tR := R.ScalarMult(tss.EC(), pf.T)
+	uG := crypto.ScalarBaseMult(tss.EC(), pf.U)
+	tRuG := tR.Add(tss.EC(), uG)
+
+	Vc := V.ScalarMult(tss.EC(), c)
+	aVc := pf.Alpha.Add(tss.EC(), Vc)
+
+	if tRuG.X().Cmp(aVc.X()) != 0 || tRuG.Y().Cmp(aVc.Y()) != 0 {
 		return false
 	}
 	return true
--- crypto/schnorr/schnorr_proof_test.go
+++ crypto/schnorr/schnorr_proof_test.go
@@ -12,9 +12,10 @@ import (
 )
 
 func TestSchnorrProof(t *testing.T) {
-	u := random.GetRandomPositiveInt(tss.EC().Params().N)
+	q := tss.EC().Params().N
+	u := random.GetRandomPositiveInt(q)
 	uG := crypto.ScalarBaseMult(tss.EC(), u)
-	proof := NewSchnorrProof(u, uG)
+	proof := NewZKProof(u, uG)
 
 	assert.True(t, proof.Alpha.IsOnCurve(tss.EC()))
 	assert.NotZero(t, proof.Alpha.X())
@@ -23,23 +24,43 @@ func TestSchnorrProof(t *testing.T) {
 }
 
 func TestSchnorrProofVerify(t *testing.T) {
-	u := random.GetRandomPositiveInt(tss.EC().Params().N)
+	q := tss.EC().Params().N
+	u := random.GetRandomPositiveInt(q)
 	X := crypto.ScalarBaseMult(tss.EC(), u)
 
-	proof := NewSchnorrProof(u, X)
+	proof := NewZKProof(u, X)
 	res := proof.Verify(X)
 
 	assert.True(t, res, "verify result must be true")
 }
 
 func TestSchnorrProofVerifyBad(t *testing.T) {
-	u  := random.GetRandomPositiveInt(tss.EC().Params().N)
-	u2 := random.GetRandomPositiveInt(tss.EC().Params().N)
+	q := tss.EC().Params().N
+	u  := random.GetRandomPositiveInt(q)
+	u2 := random.GetRandomPositiveInt(q)
 	X := crypto.ScalarBaseMult(tss.EC(), u)
 	X2 := crypto.ScalarBaseMult(tss.EC(), u2)
 
-	proof := NewSchnorrProof(u2, X2)
+	proof := NewZKProof(u2, X2)
 	res := proof.Verify(X)
 
 	assert.False(t, res, "verify result must be false")
+}
+
+func TestSchnorrVProofVerify(t *testing.T) {
+	t.Skip("WIP")
+
+	q := tss.EC().Params().N
+	k := random.GetRandomPositiveInt(q)
+	s := random.GetRandomPositiveInt(q)
+	l := random.GetRandomPositiveInt(q)
+	R := crypto.ScalarBaseMult(tss.EC(), k) // k_-1 * G
+	Rs := R.ScalarMult(tss.EC(), s)
+	lG := crypto.ScalarBaseMult(tss.EC(), l)
+	V := Rs.Add(tss.EC(), lG)
+
+	proof := NewZKVProof(V, R, s, l)
+	res := proof.Verify(V, R)
+
+	assert.True(t, res, "verify result must be true")
 }
