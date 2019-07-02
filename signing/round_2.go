9e5fb6dd061018c97e85e7d74b1e28447f3ae6ae
Luke Plaster <me@lukep.org>
2019-07-02 21:18:26 +0800

    signing: concurrent round 2 proof verification

--- signing/round_2.go
+++ signing/round_2.go
@@ -2,7 +2,6 @@ package signing
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/binance-chain/tss-lib/crypto/mta"
 	"github.com/binance-chain/tss-lib/tss"
@@ -17,44 +16,92 @@ func (round *round2) Start() *tss.Error {
 	round.started = true
 	round.resetOk()
 
+	// it's concurrency time...
+	errChs1 := make([]chan *tss.Error, len(round.Parties().Parties()))
+	for j := range errChs1 {
+		if j == round.PartyID().Index {
+			errChs1[j] = nil
+			continue
+		}
+		errChs1[j] = make(chan *tss.Error)
+	}
+	errChs2 := make([]chan *tss.Error, len(round.Parties().Parties()))
+	for j := range errChs2 {
+		if j == round.PartyID().Index {
+			errChs2[j] = nil
+			continue
+		}
+		errChs2[j] = make(chan *tss.Error)
+	}
+
 	i := round.PartyID().Index
 	for j, Pj := range round.Parties().Parties() {
 		if j == round.PartyID().Index {
 			continue
 		}
-		beta, c1ji, _, pi1ji, err := mta.BobMid(
-			round.key.PaillierPks[j],
-			round.temp.signRound1MtAInitMessages[j].Pi,
-			round.temp.gamma,
-			round.temp.signRound1MtAInitMessages[j].C,
-			round.key.NTildej[j],
-			round.key.H1j[j],
-			round.key.H2j[j],
-			round.key.NTildej[i],
-			round.key.H1j[i],
-			round.key.H2j[i])
-		if err != nil {
-			return round.WrapError(fmt.Errorf("failed to calculate bob_mid: %v", err))
+		go func(j int, Pj *tss.PartyID) {
+			beta, c1ji, _, pi1ji, err := mta.BobMid(
+				round.key.PaillierPks[j],
+				round.temp.signRound1MtAInitMessages[j].Pi,
+				round.temp.gamma,
+				round.temp.signRound1MtAInitMessages[j].C,
+				round.key.NTildej[j],
+				round.key.H1j[j],
+				round.key.H2j[j],
+				round.key.NTildej[i],
+				round.key.H1j[i],
+				round.key.H2j[i])
+			// should be thread safe as these are pre-allocated
+			round.temp.betas[j] = beta
+			round.temp.c1jis[j] = c1ji
+			round.temp.pi1jis[j] = pi1ji
+			if err == nil {
+				errChs1[j] <- nil
+				return
+			}
+			errChs1[j] <- round.WrapError(err, Pj)
+		}(j, Pj)
+		go func(j int, Pj *tss.PartyID) {
+			v, c2ji, _, pi2ji, err := mta.BobMidWC(
+				round.key.PaillierPks[j],
+				round.temp.signRound1MtAInitMessages[j].Pi,
+				round.temp.w,
+				round.temp.signRound1MtAInitMessages[j].C,
+				round.key.NTildej[j],
+				round.key.H1j[j],
+				round.key.H2j[j],
+				round.key.NTildej[i],
+				round.key.H1j[i],
+				round.key.H2j[i],
+				round.temp.bigWs[i])
+			round.temp.vs[j] = v
+			round.temp.c2jis[j] = c2ji
+			round.temp.pi2jis[j] = pi2ji
+			if err == nil {
+				errChs2[j] <- nil
+				return
+			}
+			errChs2[j] <- round.WrapError(err, Pj)
+		}(j, Pj)
+	}
+	// consume error channels; wait for goroutines
+	culprits := make([]*tss.PartyID, 0, len(round.Parties().Parties()))
+	for _, errCh := range append(errChs1, errChs2...) {
+		if errCh == nil { continue }
+		if err := <-errCh; err != nil {
+			culprits = append(culprits, err.Culprits()...)
 		}
-		v, c2ji, _, pi2ji, err := mta.BobMidWC(
-			round.key.PaillierPks[j],
-			round.temp.signRound1MtAInitMessages[j].Pi,
-			round.temp.w,
-			round.temp.signRound1MtAInitMessages[j].C,
-			round.key.NTildej[j],
-			round.key.H1j[j],
-			round.key.H2j[j],
-			round.key.NTildej[i],
-			round.key.H1j[i],
-			round.key.H2j[i],
-			round.temp.bigWs[i])
-		if err != nil {
-			return round.WrapError(fmt.Errorf("failed to calculate bob_mid_wc: %v", err))
+	}
+	if len(culprits) > 0 {
+		return round.WrapError(errors.New("failed to calculate bob_mid or bob_mid_wc"), culprits...)
+	}
+	// create and send messages
+	for j, Pj := range round.Parties().Parties() {
+		if j == round.PartyID().Index {
+			continue
 		}
-
-		round.temp.betas[j] = beta
-		round.temp.vs[j] = v
-		r2msg := NewSignRound2MtAMidMessage(Pj, round.PartyID(), c1ji, pi1ji, c2ji, pi2ji)
+		r2msg := NewSignRound2MtAMidMessage(
+			Pj, round.PartyID(), round.temp.c1jis[j], round.temp.pi1jis[j], round.temp.c2jis[j], round.temp.pi2jis[j])
 		round.temp.signRound2MtAMidMessages[round.PartyID().Index] = &r2msg
 		round.out <- r2msg
 	}
