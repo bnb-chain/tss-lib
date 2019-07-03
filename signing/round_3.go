3ecbe2e9dc66904971332d2e508f7003b5bf6364
Luke Plaster <me@lukep.org>
2019-07-03 15:53:50 +0800

    signing: concurrent proof verification in round 3

--- signing/round_3.go
+++ signing/round_3.go
@@ -2,7 +2,6 @@ package signing
 
 import (
 	"errors"
-	"fmt"
 	"math/big"
 
 	"github.com/binance-chain/tss-lib/common"
@@ -14,45 +13,86 @@ func (round *round3) Start() *tss.Error {
 	if round.started {
 		return round.WrapError(errors.New("round already started"))
 	}
-
 	round.number = 3
 	round.started = true
 	round.resetOk()
 
 	var alphas = make([]*big.Int, len(round.Parties().IDs()))
 	var us = make([]*big.Int, len(round.Parties().IDs()))
+
 	i := round.PartyID().Index
-	for j := range round.Parties().IDs() {
-		if j == round.PartyID().Index {
+
+	// it's concurrency time...
+	errChs1 := make([]chan *tss.Error, len(round.Parties().IDs()))
+	for j := range errChs1 {
+		if j == i {
+			errChs1[j] = nil
 			continue
 		}
-		alphaIj, err := mta.AliceEnd(
-			round.key.PaillierPks[i],
-			round.temp.signRound2MtAMidMessages[j].Pi1Ji,
-			round.key.H1j[i],
-			round.key.H2j[i],
-			round.temp.signRound1SentMtaInitMessages[j].C,
-			round.temp.signRound2MtAMidMessages[j].C1Ji,
-			round.key.NTildej[i],
-			round.key.PaillierSk)
-		if err != nil {
-			return round.WrapError(fmt.Errorf("failed to compute Alice_end: %v", err))
+		errChs1[j] = make(chan *tss.Error)
+	}
+	errChs2 := make([]chan *tss.Error, len(round.Parties().IDs()))
+	for j := range errChs2 {
+		if j == i {
+			errChs2[j] = nil
+			continue
 		}
-		uIj, err := mta.AliceEndWC(
-			round.key.PaillierPks[i],
-			round.temp.signRound2MtAMidMessages[j].Pi2Ji,
-			round.temp.bigWs[j],
-			round.temp.signRound1SentMtaInitMessages[j].C,
-			round.temp.signRound2MtAMidMessages[j].C2Ji,
-			round.key.NTildej[i],
-			round.key.H1j[i],
-			round.key.H2j[i],
-			round.key.PaillierSk)
-		if err != nil {
-			return round.WrapError(fmt.Errorf("failed to compute Alice_end_wc: %v", err))
+		errChs2[j] = make(chan *tss.Error)
+	}
+
+	for j, Pj := range round.Parties().IDs() {
+		if j == i {
+			continue
 		}
-		alphas[j] = alphaIj
-		us[j] = uIj
+		// Alice_end
+		go func(j int, Pj *tss.PartyID) {
+			alphaIj, err := mta.AliceEnd(
+				round.key.PaillierPks[i],
+				round.temp.signRound2MtAMidMessages[j].Pi1Ji,
+				round.key.H1j[i],
+				round.key.H2j[i],
+				round.temp.signRound1SentMtaInitMessages[j].C,
+				round.temp.signRound2MtAMidMessages[j].C1Ji,
+				round.key.NTildej[i],
+				round.key.PaillierSk)
+			alphas[j] = alphaIj
+			if err == nil {
+				errChs1[j] <- nil
+				return
+			}
+			errChs1[j] <- round.WrapError(err, Pj)
+		}(j, Pj)
+		// Alice_end_wc
+		go func(j int, Pj *tss.PartyID) {
+			uIj, err := mta.AliceEndWC(
+				round.key.PaillierPks[i],
+				round.temp.signRound2MtAMidMessages[j].Pi2Ji,
+				round.temp.bigWs[j],
+				round.temp.signRound1SentMtaInitMessages[j].C,
+				round.temp.signRound2MtAMidMessages[j].C2Ji,
+				round.key.NTildej[i],
+				round.key.H1j[i],
+				round.key.H2j[i],
+				round.key.PaillierSk)
+			us[j] = uIj
+			if err == nil {
+				errChs2[j] <- nil
+				return
+			}
+			errChs2[j] <- round.WrapError(err, Pj)
+		}(j, Pj)
+	}
+
+	// consume error channels; wait for goroutines
+	culprits := make([]*tss.PartyID, 0, len(round.Parties().IDs()))
+	for _, errCh := range append(errChs1, errChs2...) {
+		if errCh == nil { continue }
+		if err := <-errCh; err != nil {
+			culprits = append(culprits, err.Culprits()...)
+		}
+	}
+	if len(culprits) > 0 {
+		return round.WrapError(errors.New("failed to calculate Alice_end or Alice_end_wc"), culprits...)
 	}
 
 	modN := common.ModInt(tss.EC().Params().N)
