897c61d90a4ec75341de40af43dad622633b8c17
Luke Plaster <me@lukep.org>
2019-07-12 17:14:44 +0800

    tss: add regroup params

--- tss/params.go
+++ tss/params.go
@@ -1,11 +1,23 @@
 package tss
 
+import (
+	"github.com/binance-chain/tss-lib/crypto"
+)
+
 type (
 	Parameters struct {
-		partyID    *PartyID
-		parties    *PeerContext
-		partyCount int
-		threshold  int
+		partyID *PartyID
+		parties *PeerContext
+		partyCount,
+		threshold int
+	}
+
+	ReGroupParameters struct {
+		*Parameters
+		newParties *PeerContext
+		newPartyCount,
+		newThreshold int
+		bigXs []*crypto.ECPoint
 	}
 )
 
@@ -33,4 +45,54 @@ func (params *Parameters) PartyCount() int {
 
 func (params *Parameters) Threshold() int {
 	return params.threshold
+}
+
+// ----- //
+
+// Exported, used in `tss` client
+func NewReGroupParameters(ctx, newCtx *PeerContext, partyID *PartyID, partyCount, threshold, newPartyCount, newThreshold int, bigXs []*crypto.ECPoint) *ReGroupParameters {
+	params := NewParameters(ctx, partyID, partyCount, threshold)
+	return &ReGroupParameters{
+		Parameters:    params,
+		newParties:    newCtx,
+		newPartyCount: newPartyCount,
+		newThreshold:  newThreshold,
+		bigXs:         bigXs,
+	}
+}
+
+func (rgParams *ReGroupParameters) NewParties() *PeerContext {
+	return rgParams.newParties
+}
+
+func (rgParams *ReGroupParameters) NewPartyCount() int {
+	return rgParams.newPartyCount
+}
+
+func (rgParams *ReGroupParameters) NewThreshold() int {
+	return rgParams.newThreshold
+}
+
+func (rgParams *ReGroupParameters) BigXs() []*crypto.ECPoint {
+	return rgParams.bigXs
+}
+
+func (rgParams *ReGroupParameters) IsOldCommittee() bool {
+	partyID := rgParams.partyID
+	for _, Pj := range rgParams.parties.IDs() {
+		if partyID.Key.Cmp(Pj.Key) == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func (rgParams *ReGroupParameters) IsNewCommittee() bool {
+	partyID := rgParams.partyID
+	for _, Pj := range rgParams.newParties.IDs() {
+		if partyID.Key.Cmp(Pj.Key) == 0 {
+			return true
+		}
+	}
+	return false
 }
