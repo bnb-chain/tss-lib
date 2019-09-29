1ab38fbcaf7a78492fc5129ff0b60f64bfe235b5
Luke Plaster <me@gh.lukep.org>
2019-09-29 12:00:25 +0800

    add copyright comments

--- common/hash_utils.go
+++ common/hash_utils.go
@@ -1,11 +1,17 @@
+// Copyright © 2019 Binance
+//
+// This file is part of Binance. The full Binance copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
 package common
 
 import (
 	"math/big"
 )
 
-// RejectionSample implements the rejection sample logic in GG18Spec (6) Fig. 12.
-// An error may be thrown if writing to the SHA512/256 hash fails.
+// RejectionSample implements the rejection sampling logic for converting a
+// SHA512/256 hash to a value between 0-q from GG18Spec (6) Fig. 12.
 func RejectionSample(q *big.Int, eHash *big.Int) *big.Int { // e' = eHash
 	qGTZero := zero.Cmp(q) == -1
 	// e = the first |q| bits of e'
