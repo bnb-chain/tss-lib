2dd1ba7bc22087b71dd1f7dd44d91562b702958a
Luke Plaster <me@gh.lukep.org>
2019-10-17 17:42:42 +0800

    ecdsa/signing: revert temp experiments in tests

--- ecdsa/signing/round_1.go
+++ ecdsa/signing/round_1.go
@@ -38,9 +38,9 @@ func (round *round1) Start() *tss.Error {
 	// but considered different blockchain use different hash function we accept the converted big.Int
 	// if this big.Int is not belongs to Zq, the client might not comply with common rule (for ECDSA):
 	// https://github.com/btcsuite/btcd/blob/c26ffa870fd817666a857af1bf6498fabba1ffe3/btcec/signature.go#L263
-	// if round.temp.m.Cmp(tss.EC().Params().N) >= 0 {
-	// 	return round.WrapError(errors.New("hashed message is not valid"))
-	// }
+	if round.temp.m.Cmp(tss.EC().Params().N) >= 0 {
+		return round.WrapError(errors.New("hashed message is not valid"))
+	}
 
 	round.number = 1
 	round.started = true
