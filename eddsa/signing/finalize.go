1d3b571a7bd3ca29c2f0f57ed2fe056018091068
Austin <100111332+austinast@users.noreply.github.com>
2022-07-26 16:57:17 +0800

    update signature verify

--- eddsa/signing/finalize.go
+++ eddsa/signing/finalize.go
@@ -7,12 +7,15 @@
 package signing
 
 import (
+	"crypto/subtle"
 	"errors"
 	"fmt"
 	"math/big"
 
+	"github.com/agl/ed25519"
 	"github.com/agl/ed25519/edwards25519"
 	"github.com/decred/dcrd/dcrec/edwards/v2"
+	"golang.org/x/crypto/blake2b"
 
 	"github.com/binance-chain/tss-lib/tss"
 )
@@ -51,7 +54,7 @@ func (round *finalization) Start() *tss.Error {
 		Y:     round.key.EDDSAPub.Y(),
 	}
 
-	ok := edwards.Verify(&pk, round.temp.m, round.temp.r, s)
+	ok := Verify(&pk, round.temp.m, round.temp.r, s)
 	if !ok {
 		return round.WrapError(fmt.Errorf("signature verification failed"))
 	}
@@ -72,4 +75,51 @@ func (round *finalization) Update() (bool, *tss.Error) {
 
 func (round *finalization) NextRound() tss.Round {
 	return nil // finished!
+}
+
+// Verify returns true iff sig is a valid signature of message by publicKey.
+func verify(publicKey *[ed25519.PublicKeySize]byte, message []byte, sig *[ed25519.SignatureSize]byte) bool {
+	if sig[63]&224 != 0 {
+		return false
+	}
+
+	var A edwards25519.ExtendedGroupElement
+	if !A.FromBytes(publicKey) {
+		return false
+	}
+	edwards25519.FeNeg(&A.X, &A.X)
+	edwards25519.FeNeg(&A.T, &A.T)
+
+	h, _ := blake2b.New512(nil)
+	h.Write(sig[:32])
+	h.Write(publicKey[:])
+	h.Write(message)
+	var digest [64]byte
+	h.Sum(digest[:0])
+
+	var hReduced [32]byte
+	edwards25519.ScReduce(&hReduced, &digest)
+
+	var R edwards25519.ProjectiveGroupElement
+	var b [32]byte
+	copy(b[:], sig[32:])
+	edwards25519.GeDoubleScalarMultVartime(&R, &hReduced, &A, &b)
+
+	var checkR [32]byte
+	R.ToBytes(&checkR)
+	return subtle.ConstantTimeCompare(sig[:32], checkR[:]) == 1
+}
+
+// Verify verifies a message 'hash' using the given public keys and signature.
+func Verify(pub *edwards.PublicKey, hash []byte, r, s *big.Int) bool {
+	if pub == nil || hash == nil || r == nil || s == nil {
+		return false
+	}
+
+	pubBytes := pub.Serialize()
+	sig := &edwards.Signature{r, s}
+	sigBytes := sig.Serialize()
+	pubArray := copyBytes(pubBytes)
+	sigArray := copyBytes64(sigBytes)
+	return ed25519.Verify(pubArray, hash, sigArray)
 }
