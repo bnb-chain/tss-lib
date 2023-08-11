ed5c3f02926a2fac3e4973b4f370d2e9730cb97a
ycen <yycen@live.com>
2023-08-11 22:19:38 +0800

    Fix EDDSA keygen example

--- common/slice.go
+++ common/slice.go
@@ -50,3 +50,15 @@ func NonEmptyMultiBytes(bzs [][]byte, expectLen ...int) bool {
 	}
 	return true
 }
+
+// PadToLengthBytesInPlace pad {0, ...} to the front of src if len(src) < length
+// output length is equal to the parameter length
+func PadToLengthBytesInPlace(src []byte, length int) []byte {
+	oriLen := len(src)
+	if oriLen < length {
+		for i := 0; i < length-oriLen; i++ {
+			src = append([]byte{0}, src...)
+		}
+	}
+	return src
+}
