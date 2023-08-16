e18468b4f25109fb5031174e1e66c8dfa01b9cfa
asdfsx <asdfsx@gmail.com>
2023-08-16 17:55:54 +0800

    upgrade dependencies version

--- crypto/ckd/child_key_derivation_test.go
+++ crypto/ckd/child_key_derivation_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	. "github.com/bnb-chain/tss-lib/crypto/ckd"
-	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/btcec/v2"
 )
 
 func TestPublicDerivation(t *testing.T) {
